ed5e8d3e5879373fd6edba1b4ad499bd451bbae3
Michael Rice <michael.rice@rackspace.com>
2014-10-14 23:39:37 -0500

    minor cosmetic stuff to forms.py

--- vBurgundy/user/forms.py
+++ vBurgundy/user/forms.py
@@ -1,24 +1,39 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField
+from wtforms import PasswordField, StringField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 from .models import User
 
+
 class RegisterForm(Form):
-    username = TextField('Username',
-                    validators=[DataRequired(), Length(min=3, max=25)])
-    email = TextField('Email',
-                    validators=[DataRequired(), Email(), Length(min=6, max=40)])
+    """
+    Registration Form for new users.
+    """
+    username = StringField('Username',
+                           validators=[DataRequired(),
+                           Length(min=3, max=25)])
+    email = StringField('Email',
+                        validators=[DataRequired(),
+                        Email(), Length(min=6, max=40)])
     password = PasswordField('Password',
-                                validators=[DataRequired(), Length(min=6, max=40)])
+                             validators=[DataRequired(),
+                             Length(min=6, max=40)])
     confirm = PasswordField('Verify password',
-                [DataRequired(), EqualTo('password', message='Passwords must match')])
+                            [DataRequired(),
+                             EqualTo('password',
+                                     message='Passwords must match')])
 
     def __init__(self, *args, **kwargs):
         super(RegisterForm, self).__init__(*args, **kwargs)
         self.user = None
 
     def validate(self):
+        """
+        Validate if the user is a valid new user or not.
+
+        :return:
+        :rtype boolean:
+        """
         initial_validation = super(RegisterForm, self).validate()
         if not initial_validation:
             return False

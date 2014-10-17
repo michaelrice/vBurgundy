24bb83e9079a481ab81a8603afa464337dd2c862
Michael Rice <michael.rice@rackspace.com>
2014-10-16 22:48:48 -0500

    added vclookup model

--- vBurgundy/vclookup/models.py
+++ vBurgundy/vclookup/models.py
@@ -11,3 +11,25 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+
+import datetime as dt
+
+from vBurgundy.database import (
+    Column,
+    db,
+    Model,
+)
+
+
+class Vcenter(Model):
+
+    __tablename__ = 'vcenter'
+    vcenter_name = Column(db.String(length=120), unique=True, nullable=False)
+    vcenter_ip = Column(db.String(15), nullable=True)
+    vcenter_user = Column(db.String(120), unique=False, nullable=True)
+    vcenter_user_pwd = Column(db.String(240), nullable=True)
+    created_at = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
+
+    def __init__(self, vcname, vcip=None, vcuser=None, vcpwd=None, **kwargs):
+        db.Model(vcenter_name=vcname, vcenter_ip=vcip, vcenter_user=vcuser,
+                 vcenter_user_pwd=vcpwd, **kwargs)
